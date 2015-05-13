097a327b3b54de13bda0494fa49e956bf0ee48ec
Lindsey <lmdragun@gmail.com>
2015-05-13 14:08:26 -0400

    notes

--- w08/d03_ooj_ajax_review/exercises/trillo_solution/js/views/trillo.js
+++ w08/d03_ooj_ajax_review/exercises/trillo_solution/js/views/trillo.js
@@ -1,14 +1,19 @@
 var TrilloView = function(trilloModel){
+  //these find the different elements to be used within the document
   var newCardButton = document.querySelector("#new-card-button")
   this.newCardText = document.querySelector("#new-card-text")
   this.toDoList = document.querySelector("#todo-column .card-list")
   this.doneList = document.querySelector("#completed-column .card-list")
+  //this declares the trilloModel as the model of this TrilloView
   this.model = trilloModel
+  //this adds an event listener to the button
   newCardButton.addEventListener("click", this.addCard.bind(this))
+  //this renders everything in the TrilloView
   this.render()
 }
 
 TrilloView.prototype = {
+  //this adds a card by initializing a new Card and saving it to the Rails app
   addCard: function(event){
     event.preventDefault();
     var description = this.newCardText.value;
