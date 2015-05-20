04b2456444c4ad5b55d04e195357f9ca237407d5
Lindsey <lmdragun@gmail.com>
2015-05-20 16:05:47 -0400

    class work

--- w10/d03_warmup/js/src/pokemon.js
+++ w10/d03_warmup/js/src/pokemon.js
@@ -3,20 +3,27 @@ var pokemon = {
   pokemonList : allPokemon,
 
   printAllPokemonNamesToConsole : function(){
+    _.each(allPokemon, function(poke){
+      console.log(poke.name);
+    });
   },
 
   findPokemonByName : function(name){
+    return _.findWhere(allPokemon, {name:name})
+
   },
 
   findStrongestPokemon : function(){
+    return _.max(allPokemon, function(poke){
+      return parseInt(poke.stats.spattack)
+    })
   },
 
   findPokemonByType : function(type){
+   return _.findWhere(allPokemon, {type:type})
   },
 
   findAllTypes : function(){
-  },
-
-  totalStats : function(name){
   }
+
 }
